4401cf6fed547e98a43fbcb5106ef6a7737727fc
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-10-20 23:56:55 +0530

    route to all the page components and navbar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import './App.css';
+import './assets/styles.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticleListPage from './pages/ArticleListPage';
+import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
+import Navbar from './components/Navbar';
 
 const App = () => {
-	return <></>;
+	return (
+		<>
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<HomePage />} exact />
+					<Route path="/about" element={<AboutPage />} exact />
+					<Route
+						path="/articleList"
+						element={<ArticleListPage />}
+						exact
+					/>
+					<Route
+						path="/article/:articleId"
+						element={<ArticlePage />}
+						exact
+					/>
+					<Route
+						path="/page_not_found"
+						element={<NotFoundPage />}
+						exact
+					/>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 };
 
 export default App;
