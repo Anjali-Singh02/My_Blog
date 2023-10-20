e0173c274d9d5a7bf8c47ed78154059d6d8851fd
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-10-20 23:55:49 +0530

    list of articles displayed

--- src/pages/ArticleListPage.jsx
+++ src/pages/ArticleListPage.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
-
+import articles from './article-content';
+import { Link } from 'react-router-dom';
 const ArticleListPage = () => {
-	return <div>ArticleListPage</div>;
+	return (
+		<>
+			<section className="article-list-container">
+				<div className="title-container">
+					<h1>Articles</h1>
+				</div>
+				{articles.map((article) => (
+					<div className="article-list-content">
+						<Link to={`/article/${article.name}`}>
+							<h2>{article.title}</h2>
+							<p>{article.content[0].substring(0, 150)}...</p>
+						</Link>
+					</div>
+				))}
+			</section>
+		</>
+	);
 };
 
 export default ArticleListPage;
