d4ffc78a61852642e153c5125c13bf32241423bb
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-10-20 23:55:04 +0530

    file containing articles list

---
+++ src/pages/article-content.js
@@ -0,0 +1,31 @@
+const articles = [
+	{
+		name: 'learn-react',
+		title: 'The fastest way to learn react',
+		content: [
+			`The most critical and challenging choice of a business owner is to build their enterprise application in such a manner that it is easy to maintain, can be reusable, delivers high performance, and the most essential is that the app must have a good scope of scalability.`,
+
+			`React State Management is a striking topic in the web development domain, and when you have a React.js enterprise application, getting an in-depth grasp of the same is vital. As we know the pain points of business app development, let us check how state management React libraries can enable your enterprise app to match your business aims.`,
+		],
+	},
+	{
+		name: 'learn-node',
+		title: 'The fastest way to learn node',
+		content: [
+			`The most critical and challenging choice of a business owner is to build their enterprise application in such a manner that it is easy to maintain, can be reusable, delivers high performance, and the most essential is that the app must have a good scope of scalability.`,
+
+			`React State Management is a striking topic in the web development domain, and when you have a React.js enterprise application, getting an in-depth grasp of the same is vital. As we know the pain points of business app development, let us check how state management React libraries can enable your enterprise app to match your business aims.`,
+			`React State Management is a striking topic in the web development domain, and when you have a React.js enterprise application, getting an in-depth grasp of the same is vital. As we know the pain points of business app development, let us check how state management React libraries can enable your enterprise app to match your business aims.`,
+		],
+	},
+	{
+		name: 'learn-mongodb',
+		title: ' learn MongoDB',
+		content: [
+			`The most critical and challenging choice of a business owner is to build their enterprise application in such a manner that it is easy to maintain, can be reusable, delivers high performance, and the most essential is that the app must have a good scope of scalability.`,
+
+			`React State Management is a striking topic in the web development domain, and when you have a React.js enterprise application, getting an in-depth grasp of the same is vital. As we know the pain points of business app development, let us check how state management React libraries can enable your enterprise app to match your business aims.`,
+		],
+	},
+];
+export default articles;
